0105e35d4444bbc6760363371b979e8c57ffcfaf
BoolInt <atc_tyrann@hotmail.com>
2016-09-25 14:49:34 +0200

    x

--- build.bat
+++ build.bat
@@ -15,6 +15,11 @@ REM Build
 "%programfiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" %project%.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
 if not "%errorlevel%"=="0" goto failure
 
+REM Unit tests
+call %nuget% install NUnit.Runners -Version 3.4.1 -OutputDirectory packages
+packages\NUnit.Runners.3.4.1\tools\nunit-console.exe /config:%config% /framework:net-4.6.1 %project%.Test\bin\%config%\%project%.Test.dll
+if not "%errorlevel%"=="0" goto failure
+
 REM Package
 REM Die Paket-Informationen werden aus der project.json gezogen. Diese ist aber nicht aktuell (Version)
 REM mkdir %cd%\Artifacts
