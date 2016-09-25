59ffa6a7a76cb72add2974b336d397cc326043c8
BoolInt <atc_tyrann@hotmail.com>
2016-09-25 15:43:34 +0200

    x

--- build.bat
+++ build.bat
@@ -7,30 +7,24 @@ if "%config%" == "" (
    set config=Release
 )
 
-REM Restore
+REM Restore Packages by DotNet
 call dotnet restore
 if not "%errorlevel%"=="0" goto failure
 
-REM AssemblyVersion
+REM Update AssemblyInfo by GitVersion
 call %GitVersion% /updateassemblyinfo true
 if not "%errorlevel%"=="0" goto failure
 
-REM Build
+REM Build Solution by MSBuild
 "%programfiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" %project%.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
 if not "%errorlevel%"=="0" goto failure
 
-REM Unit tests
+REM Run Unittests by NUnit
 call %nuget% install NUnit.Runners -Version 3.4.1 -OutputDirectory packages
 packages\NUnit.Runners.3.4.1\tools\nunit-console.exe /config:%config% /framework:net-4.6.1 %project%.Test\bin\%config%\%project%.Test.dll
 if not "%errorlevel%"=="0" goto failure
 
-REM Package
-REM Die Paket-Informationen werden aus der project.json gezogen. Diese ist aber nicht aktuell (Version)
-REM mkdir %cd%\Artifacts
-REM call dotnet pack %project% --configuration %config% --output Artifacts
-REM if not "%errorlevel%"=="0" goto failure
-
-REM Pack
+REM Pack NuSpec-Packages by NuGet
 set version=
 if not "%PackageVersion%" == "" (
    set version=-Version %PackageVersion%
