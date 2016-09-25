01bf7e0f57e60181795112ab7f34e64bc0c0b5ff
BoolInt <atc_tyrann@hotmail.com>
2016-09-25 12:22:12 +0200

    x

---
+++ Bqpl.MicroKernel.Test/DomainTest.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bqpl.MicroKernel.Test
+{
+  [TestFixture]
+  public class DomainTest
+  {
+    [Test]
+    public void DomainsNotSame()
+    {
+      var domain = Domain.CreateInstance();
+      var domain2 = Domain.CreateInstance();
+      Assert.AreNotSame(domain, domain2);
+    }
+
+    [Test]
+    public void Sample()
+    {
+      var domain = Domain.CreateInstance();
+      Assert.NotNull(domain);
+    }
+  }
+}
---
+++ Bqpl.MicroKernel/Domain.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Practices.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bqpl.MicroKernel
+{
+  public class Domain
+  {
+    private UnityContainer unityContainer;
+
+    private Domain()
+    {
+    }
+
+    private UnityContainer UnityContainer
+    {
+      get
+      {
+        return Lazy.GetOrCreate(ref unityContainer);
+      }
+    }
+
+    public static Domain CreateInstance()
+    {
+      var domain = new Domain();
+      domain.Initialize();
+      return domain;
+    }
+
+    public T Resolve<T>()
+    {
+      return UnityContainer.Resolve<T>();
+    }
+
+    private void Initialize()
+    {
+      UnityContainer.RegisterInstance(UnityContainer);
+    }
+  }
+}
